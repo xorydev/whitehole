b77f253b2b3e560a5af7e73e7ca3aab608e99ec7
Xory <xorycode@shuriken.pm>
2024-10-27 19:39:54 +0200

    feat: tokioification pt 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,19 @@
-use std::path::Path;
-use std::net::{TcpStream, Shutdown};
-use std::io::{Read, Write};
+use tokio::net::TcpStream;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
 use std::fs;
 
-pub fn handle_client(mut connection: TcpStream, target_dir: &String) -> std::io::Result<()> {
+pub async fn handle_client(mut connection: TcpStream, target_dir: &String) -> std::io::Result<()> {
     let mut buffer = vec![0 as u8; 1024]; // Little bit of wiggle room.
-    connection.read(&mut buffer)?;
+    connection.read(&mut buffer).await?;
     let header = String::from_utf8_lossy(&buffer).to_string();
     println!("{header}");
     if !header.starts_with("GET") {
-        connection.shutdown(Shutdown::Both)?;
         println!("ion wanna hear it");
         return Ok(())
     }
     let client_desired_file_path = format!("{}/{}", target_dir, &header.split(" ").collect::<Vec<&str>>()[1][1..]); // Your code is not optimised if it doesn't make an inexperienced rustdev have a heart attack.
     if client_desired_file_path.contains("..") {
-        connection.shutdown(Shutdown::Both)?;
         println!("You should kill yourself... NOW!");
         return Ok(())
     }
@@ -25,6 +23,6 @@ pub fn handle_client(mut connection: TcpStream, target_dir: &String) -> std::io:
     let mut response: Vec<u8> = String::from("HTTP/1.1 200 OK").into_bytes();
     let mut file_contents = fs::read(&client_desired_file_path)?;
     response.append(&mut file_contents);
-    connection.write(&response)?;
+    connection.write(&response).await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::net;
+use tokio::net;
 use std::path::Path;
 use whitehole::handle_client;
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     let args = &std::env::args().collect::<Vec<String>>()[1..];
     let target_dir = Path::new(&args[0]);
     if !target_dir.is_dir() {
         panic!("Target is not a directory");
     }
 
-    let listener = net::TcpListener::bind("0.0.0.0:23898")?; // Alphanumerical for "WHIH" or
+    let listener = net::TcpListener::bind("0.0.0.0:23898").await?; // Alphanumerical for "WHIH" or
                                                              // "Whitehole". No idea why I did
                                                              // this.
-    for connection in listener.incoming() {
-        handle_client(connection?, &args[0])?; // See lib.rs for handle_client()
+    loop { 
+        let (socket, _) = listener.accept().await?;
+        handle_client(socket, &target_dir.to_str().unwrap().to_string()).await?;
     }
-    Ok(())
 }
